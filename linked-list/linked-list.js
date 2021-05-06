bec257522cb83fe7f993eb215f378a37cd29de64
Ruban Prakash <rubanprakash@hotmail.com>
2021-05-06 13:07:13 +0100

    first commit

---
+++ linked-list/linked-list.js
@@ -0,0 +1,21 @@
+class Node {
+  constructor(value, next, prev) {
+    this.value=value;
+    this.next=next||null;
+    this.prev=prev||null;
+  }
+}
+
+class LinkedList {
+  constructor(){
+    this.head=null;
+    this.tail=null;
+  }
+  append(){
+    if(!this.head){
+      this.head = this.tail = new Node
+    }
+  }
+}
+
+const list = new LinkedList();
